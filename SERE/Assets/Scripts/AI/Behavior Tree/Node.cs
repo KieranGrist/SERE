68a8ac0b78e36a841e657e7713b02d36da159905
Kieran Grist <kierangrist@gmail.com>
2020-02-11 10:52:12 +0000

    Started Behavior Tree

---
+++ SERE/Assets/Scripts/AI/Behavior Tree/Node.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+//https://hub.packtpub.com/building-your-own-basic-behavior-tree-tutorial/
+
+public enum NodeStatus
+{
+    RUNNING,
+    SUCCESS,
+    FAILURE
+}
+
+[System.Serializable]
+public abstract class Node : ScriptableObject
+{
+    public BehaviourTree Bt;
+    public Node(BehaviourTree Bt)
+    {
+        this.Bt = Bt;
+    }
+    public abstract NodeStatus Execute();
+
+    public virtual void Reset()
+    {
+
+    }
+}
